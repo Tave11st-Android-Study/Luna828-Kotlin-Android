120e49e19ff618f5316eb7c7bb5902ccf7681022
luna <eskw0701@naver.com>
2023-03-21 22:44:25 +0900

    [intent] 프로젝트는 파일을 붙여넣기하면서 Default Activity Not Found가 나옴으로 앱 생성 실패 ->
    새로운 프로젝트 파일 [TAVE_Android]로 다시 복습할 예정

---
+++ Tave_Android/app/src/main/java/com/example/tave_android/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.tave_android
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tave_android.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    val TAG: String = "로그"
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btn.setOnClickListener {
+            //Intent 사용 방법
+            val intent = Intent(this, IntentActivity::class.java)
+            startActivity(intent)
+        }
+
+//        binding.EditText.setOnClickListener {
+//            val intent = Intent(this, EditTextActivity::class.java)
+//            startActivity(intent)
+//        }
+    }
+}
