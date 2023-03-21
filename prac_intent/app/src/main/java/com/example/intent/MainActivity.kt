2792b168ffe265daf1ac028749de084c38e73dd6
luna <eskw0701@naver.com>
2023-03-21 22:32:05 +0900

    ?

--- prac_intent/app/src/main/java/com/example/intent/MainActivity.kt
+++ prac_intent/app/src/main/java/com/example/intent/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.practice_intent
+package com.example.intent
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import com.example.intent.EditTextActivity
 import com.example.intent.databinding.ActivityMainBinding
+import com.example.practice_intent.SecondActivity
 
 class MainActivity : AppCompatActivity() {
 
