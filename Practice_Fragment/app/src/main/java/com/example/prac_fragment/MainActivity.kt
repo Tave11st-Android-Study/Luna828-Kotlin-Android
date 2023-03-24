d943e393eeaffd23765d14b8cb660f9c580ea016
luna <eskw0701@naver.com>
2023-03-24 22:57:29 +0900

    [Practice_Fragment] fragment A 와 B를 나누어 add해서 메인화면에 나타내 보기
    * 에러 사항 : 다시 A 버튼과 B버튼을 누를 시 화면이 꺼지는 현상 (중복해서 누르는 것이 안됨 1번만 눌러짐)

--- Practice_Fragment/app/src/main/java/com/example/prac_fragment/MainActivity.kt
+++ Practice_Fragment/app/src/main/java/com/example/prac_fragment/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.prac_fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.prac_fragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val aFragment by lazy { AFragment() }
+    private val bFragment by lazy { BFragment() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        with(binding) {
+            BtnA.setOnClickListener {
+                GoAFragment()
+            }
+
+            BtnB.setOnClickListener {
+                GoBFragment()
+            }
+        }
+    }
+
+    fun GoAFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fragmentList, aFragment)
+        transaction.addToBackStack("a")
+        transaction.commit()
+    }
+
+    fun GoBFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fragmentList, bFragment)
+        transaction.addToBackStack("b")
+        transaction.commit()
     }
 }
