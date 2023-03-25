d7f7f4423b33462b5066b5455967baf6fdb96466
luna <eskw0701@naver.com>
2023-03-25 18:25:04 +0900

    [TAVE_Android] 커스텀뷰 만들어서 도형만들기 (원, 네모)

--- Tave_Android/app/src/main/java/com/example/tave_android/CustomView.kt
+++ Tave_Android/app/src/main/java/com/example/tave_android/CustomView.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Rect
+import android.graphics.RectF
 import android.view.View
 
 class CustomView(context: Context) : View(context){
@@ -19,6 +21,30 @@ class CustomView(context: Context) : View(context){
         //그림을 그려주기 = canvas
         canvas?.run {
             drawText("안녕하세요", 0f, 100f, paint) //0f, 0f 텍스트의 위치
+            customDrawCircle(canvas)
+            customDrawRect(canvas)
         }
+    }
+
+    //함수를 빼는 방법
+    fun customDrawCircle(canvas: Canvas) {
+        val paint = Paint()
+        paint.style = Paint.Style.FILL
+        paint.color = Color.BLUE
+
+        canvas.drawCircle(150f, 300f, 100f, paint)
+    }
+
+    fun customDrawRect(canvas: Canvas) {
+        val paint = Paint()
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = 20f
+        paint.color = Color.GREEN
+
+        val rect = Rect(50,450, 250, 650)
+        val rect2 = RectF(50f,450f, 250f, 650f)
+
+        canvas.drawRect(rect, paint)
+        canvas.drawRect(rect2, paint)
     }
 }
