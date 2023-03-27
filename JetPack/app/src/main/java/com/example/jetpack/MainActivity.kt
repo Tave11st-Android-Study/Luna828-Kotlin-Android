03a279b68ab210cd04b4588da1926f3cb06f3008
luna <eskw0701@naver.com>
2023-03-27 23:03:59 +0900

    [JetPack] 외부에서 Favorite 값을 할당 받는 방법과 callback 함수 사용하기

--- JetPack/app/src/main/java/com/example/jetpack/MainActivity.kt
+++ JetPack/app/src/main/java/com/example/jetpack/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,22 +33,29 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ImageCard()
+            var isFavorite by rememberSaveable { //by 위임 기능..? , by 쓰는 방법 import androidx.compose.runtime.getValue / setValue를 직접 import해주면 가능
+                mutableStateOf(false) //보존이 필요한 부분에서는 rememberSaveable사용하면됨
+            }
+            ImageCard(
+                isFavorite = isFavorite,
+                modifier = Modifier.fillMaxWidth(0.5f).padding(16.dp), //비율
+            ){
+                favorite ->
+                isFavorite = favorite
+            }
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ImageCard() {
-    var isFavorite by remember { //by 위임 기능..? , by 쓰는 방법 import androidx.compose.runtime.getValue / setValue를 직접 import해주면 가능
-        mutableStateOf(false)
-    }
-
+fun ImageCard(
+    modifier: Modifier = Modifier,
+    isFavorite: Boolean, //변수가 아니라 상수이기 때문에 callback 함수를 만들어줘야함 
+    onTabFavorite: (Boolean) -> Unit, //callback , Unit return이 없는 형태
+) {
     Card(
-        modifier = Modifier
-            .fillMaxWidth(0.5f)
-            .padding(16.dp), //비율
+        modifier = modifier,
         shape = RoundedCornerShape(8.dp),
         elevation = CardDefaults.cardElevation(5.dp)
     ) {
@@ -65,7 +73,7 @@ fun ImageCard() {
                 contentAlignment = Alignment.TopEnd,
             ){
                 IconButton(onClick = {
-                    isFavorite = !isFavorite
+                    onTabFavorite(!isFavorite)
                 }) {
                     Icon(
                         imageVector = if(isFavorite)Icons.Default.Favorite else Icons.Default.FavoriteBorder,
