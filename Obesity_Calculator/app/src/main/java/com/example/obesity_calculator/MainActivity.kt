5d34430a62f6a7276a7b4257d49cf0f82ef17590
luna <eskw0701@naver.com>
2023-03-30 00:11:14 +0900

    [Obesity_Calculator] 다시한번 클론코딩하면서 Data 흐름 분석 및 compose 공부 (내일 한번 더 복습하기)

--- Obesity_Calculator/app/src/main/java/com/example/obesity_calculator/MainActivity.kt
+++ Obesity_Calculator/app/src/main/java/com/example/obesity_calculator/MainActivity.kt
@@ -34,26 +34,144 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val viewModel = viewModel<BmiViewModel>()
+            val viewModel = viewModel<BmiViewModel2>()
             val navController = rememberNavController()
 
-            val bmi = viewModel.bmi.value //state 값을 가짐
+            val bmi = viewModel.bmi.value
 
             NavHost(navController = navController, startDestination = "home") {
-                composable(route = "home") {
-                    HomeScreen(){ height, weight ->
-                        viewModel.bimCalculate(height,weight)
-                        navController.navigate("result")
-
-                    }
+                composable("home") {
+                    HomeScreen2(
+                        navController = navController,
+                        onResultClicked = { height, weight ->
+                            viewModel.bmiCal(height, weight)
+                            navController.navigate("result")
+                        }) //onResultClicked가 Data를 받아와야하니까.. 데이터를 담을 ViewModel 작성
                 }
-                composable(route = "result") {
-                    ResultScreen(navController = navController, bmi = bmi)
+                composable("result") {
+                    ResultScreen2(navController = navController, bmi = bmi)
                 }
             }
         }
     }
 }
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun HomeScreen2(
+    navController: NavController,
+    onResultClicked: (Double, Double) -> Unit
+) {
+    val (height, setHeight) = rememberSaveable {
+        mutableStateOf("")
+    }
+
+    val (weight, setWeight) = rememberSaveable {
+        mutableStateOf("")
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "비만도 계산기") })
+        }
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            OutlinedTextField(
+                value = height,
+                onValueChange = setHeight,
+                label = { Text(text = "키") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+            Spacer(modifier = Modifier.padding(8.dp))
+            OutlinedTextField(
+                value = weight,
+                onValueChange = setWeight,
+                label = { Text(text = "몸무게") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+            Spacer(modifier = Modifier.padding(8.dp))
+            Button(
+                onClick = {
+                    if (height.isNotEmpty() && weight.isNotEmpty()) {
+                        onResultClicked(height.toDouble(), weight.toDouble())
+                    }
+                },
+                modifier = Modifier.align(Alignment.End)
+            ) {
+                Text(text = "클릭")
+            }
+        }
+    }
+}
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun ResultScreen2(navController: NavController, bmi: Double) {
+
+    val text = when {
+        bmi >= 35 -> "고도 비만"
+        bmi >= 30 -> "2단계 비만"
+        bmi >= 25 -> "1단계 비만"
+        bmi >= 23 -> "과체중"
+        bmi >= 18.5 -> "정상"
+        else -> "저체중"
+    }
+
+    val imageRes = when {
+        bmi >= 23 -> R.drawable.baseline_sentiment_very_dissatisfied_24
+        bmi >= 18.5 -> R.drawable.baseline_sentiment_satisfied_24
+        else -> R.drawable.baseline_sentiment_dissatisfied_24
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "비만도 계산기") },
+                navigationIcon = {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "home",
+                        modifier = Modifier.clickable {
+                            navController.popBackStack()
+                        }
+                    )
+                }
+            )
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(100.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(text = text, fontSize = 50.sp)
+            Spacer(modifier = Modifier.height(50.dp))
+            Image(
+                painter = painterResource(id = imageRes),
+                contentDescription = null,
+                modifier = Modifier.size(200.dp),
+            )
+        }
+    }
+}
+
+class BmiViewModel2 : ViewModel() {
+    private val _bmi = mutableStateOf(0.0)
+    val bmi: State<Double> = _bmi
+
+    fun bmiCal(height: Double, weight: Double) {
+        _bmi.value = weight / (height / 100).pow(2.0)
+    }
+}
+
+// **************************************************************************보지 않고 코딩******************************************************************
+
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
@@ -178,5 +296,21 @@ class BmiViewModel : ViewModel() {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
+    val viewModel = viewModel<BmiViewModel>()
+    val navController = rememberNavController()
 
+    val bmi = viewModel.bmi.value //state 값을 가짐
+
+    NavHost(navController = navController, startDestination = "home") {
+        composable(route = "home") {
+            HomeScreen() { height, weight ->
+                viewModel.bimCalculate(height, weight)
+                navController.navigate("result")
+
+            }
+        }
+        composable(route = "result") {
+            ResultScreen(navController = navController, bmi = bmi)
+        }
+    }
 }
