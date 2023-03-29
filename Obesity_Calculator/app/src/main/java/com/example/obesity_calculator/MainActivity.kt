65a62e093010cbca7cd8ce454f124a16c9f871bf
luna <eskw0701@naver.com>
2023-03-29 14:51:59 +0900

    [Obesity_Calculator] Vector Asset 의 기본 이모티콘 추가해보기, 두번째 화면 완성

--- Obesity_Calculator/app/src/main/java/com/example/obesity_calculator/MainActivity.kt
+++ Obesity_Calculator/app/src/main/java/com/example/obesity_calculator/MainActivity.kt
@@ -1,22 +1,26 @@
 package com.example.obesity_calculator
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.obesity_calculator.ui.theme.Obesity_CalculatorTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -71,8 +75,36 @@ fun HomeScreen(name: String) {
     }
 }
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun ResultScreen(bmi: Double){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "비만도 계산기")}
+            )
+        }
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(50.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text("과체중", fontSize = 30.sp)
+            Spacer(modifier = Modifier.height(50.dp))
+            Image(
+                painter = painterResource(id = R.drawable.baseline_sentiment_dissatisfied_24),
+                contentDescription = null,
+                modifier = Modifier.size(100.dp),
+                //colorFilter = (ColorFilter.tint(color = ))
+            )
+
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-
+    ResultScreen(bmi = 50.0)
 }
