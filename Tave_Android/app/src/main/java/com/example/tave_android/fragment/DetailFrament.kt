eadecf4cd28320577a66ce60a1d6377aac21ac29
luna <eskw0701@naver.com>
2023-03-29 22:55:46 +0900

    [TAVE_Android] View Binding onDestroy() 추가하기

--- Tave_Android/app/src/main/java/com/example/tave_android/fragment/DetailFrament.kt
+++ Tave_Android/app/src/main/java/com/example/tave_android/fragment/DetailFrament.kt
@@ -12,7 +12,9 @@ import com.example.tave_android.databinding.FragmentDetailFramentBinding
 import com.example.tave_android.databinding.FragmentListBinding
 
 class DetailFrament : Fragment() {
-    lateinit var binding: FragmentDetailFramentBinding
+    private var _bingding: FragmentDetailFramentBinding? = null
+
+    private val binding get() = _bingding!!
     lateinit var fragmentActivity: FragmentActivity
 
     override fun onAttach(context: Context) { //context : 나를 삽입한 액티비티가 담김
@@ -25,7 +27,7 @@ class DetailFrament : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentDetailFramentBinding.inflate(inflater, container, false)
+        _bingding = FragmentDetailFramentBinding.inflate(inflater, container, false)
 
         return binding.root
     }
@@ -36,6 +38,14 @@ class DetailFrament : Fragment() {
         binding.btnBack.setOnClickListener {
             fragmentActivity.goBack()
         }
+    }
+
+    //Fragment에서 View Binding을 사용할 경우 Fragment는 View보다 오래 지속되어, Fragment Lifecyle로 인해 메모리 누수가 발생할 수 있음
+    //그래서 반드시 binding 변수를 onDestroyView() 이후에 null로 만들어 줘야함!
+    //Fragment 재사용을 위해 View들을 메모리에 보관하도록 Fragment의 동작 변경
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _bingding = null
     }
 
 }
