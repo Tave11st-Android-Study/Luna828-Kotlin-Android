1627c1dae54c05db565ed7a6d0f5f8449cdf1132
luna <eskw0701@naver.com>
2023-03-30 21:30:46 +0900

    [StopWatch] build.gradle에 dependencies duplicate 에러 수정

--- StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -17,8 +19,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.stopwatch.viewModel.MainViewModel
+import java.util.*
+import kotlin.concurrent.timer
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -150,4 +154,54 @@ fun DefaultPreview() {
         },
         onLapTime = { viewModel.recordLapTime() }
     )
+}
+
+//기능을 작성할 때 - ViewModel()
+class MainViewModel: ViewModel(){
+    private var time = 0
+    private var timerTask: Timer? = null //타임을 멈추는 경우에 대비해서 null을 해줘야함
+
+    private val _isRunning = mutableStateOf(false)
+    val isRunning: State<Boolean> = _isRunning
+
+    private val _sec = mutableStateOf(0)
+    val sec: State<Int> = _sec
+
+    private val _milli = mutableStateOf(0)
+    val milli: State<Int> = _milli
+
+    private var lap = 1 //랩 타임 도장 찍기
+    private val _lapTimes = mutableStateOf(mutableListOf<String>())
+    val lapTimes: State<List<String>> = _lapTimes
+
+    fun start() {
+        _isRunning.value = true
+        timerTask = timer(period = 10) { //period: 0.001초
+            time++
+            _sec.value = time / 100
+            _milli.value = time % 100
+        }
+    }
+
+    fun pause() {
+        _isRunning.value = false
+        timerTask?.cancel()
+    }
+
+    fun reset() {
+        timerTask?.cancel()
+
+        time = 0 //time 초기화 필요
+        _isRunning.value = false
+        _sec.value = 0
+        _milli.value = 0
+
+        _lapTimes.value.clear()
+        lap = 1
+    }
+
+    fun recordLapTime() {
+        _lapTimes.value.add(0, "$lap LAP : ${sec.value}.${milli.value}")
+        lap++
+    }
 }
