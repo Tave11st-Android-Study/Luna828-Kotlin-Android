d137404a0f72aedba1badd310593b675eb47155e
luna <eskw0701@naver.com>
2023-04-12 23:17:59 +0900

    [MyGallery] Ui작성하기

--- MyGallery/app/src/main/java/com/example/mygallery/MainActivity.kt
+++ MyGallery/app/src/main/java/com/example/mygallery/MainActivity.kt
@@ -11,17 +11,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import coil.compose.rememberImagePainter
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.HorizontalPagerIndicator
+import com.google.accompanist.pager.rememberPagerState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,13 +42,14 @@ class MainActivity : ComponentActivity() {
             //권한요청
             val launcher =
             // rememberLauncherForActivityResult 사용자 인터랙션에 대한 결과를 처리하기 위해 제공되는 API 중 하나
-            //ActivityResultContracts.RequestPermission() -> 권한 요청 결과를 처리하기 위한 Contract입니다. Contract는 ActivityResult API와 함께 사용됩니다. 이 Contract를 사용하면 권한 요청 시 사용자의 동의 여부에 대한 결과를 처리할 수 있습니다.
+                //ActivityResultContracts.RequestPermission() -> 권한 요청 결과를 처리하기 위한 Contract입니다. Contract는 ActivityResult API와 함께 사용됩니다. 이 Contract를 사용하면 권한 요청 시 사용자의 동의 여부에 대한 결과를 처리할 수 있습니다.
                 rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
                     granted = isGranted
                 }
 
             //권한이 있는지 체크 코드
-            if (ContextCompat.checkSelfPermission(  this, Manifest.permission.READ_EXTERNAL_STORAGE, //사진을 가지고 오는 것..
+            if (ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.READ_EXTERNAL_STORAGE, //사진을 가지고 오는 것..
                 ) == PackageManager.PERMISSION_GRANTED //권한이 수락되었는지를 묻는 것
             ) {
                 granted = true
@@ -111,5 +122,42 @@ fun PermissionRequestScreen(
         Button(onClick = onClick) {
             Text("권한 요청")
         }
+    }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun HomeScreen(photoUris: List<Uri>) {
+    val pagerState = rememberPagerState()
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        HorizontalPager(
+            //현재 위치를 기억하기위한 상태
+            state = pagerState,
+            count = photoUris.size,
+            modifier = Modifier
+                .weight(1f)
+                .padding(16.dp)
+                .fillMaxSize(),
+        ) { pageIndex ->
+            Card() {
+                Image(
+                    painter = rememberImagePainter(
+                        //coil에서 제공함
+                        data = photoUris[pageIndex],
+                    ),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop, //전체적으로 이미지가 꽉차 보이게 하는 거..
+                )
+            }
+        }
+
+        HorizontalPagerIndicator(
+            pagerState = pagerState,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(16.dp)
+        )
     }
 }
